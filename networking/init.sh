af0acd4ef7b5f1ddd20cd70106e603643654e633
Vaclav Klecanda <vencax77@gmail.com>
2013-12-02 22:06:29 +0100

    settings not by params but from env

--- networking/init.sh
+++ networking/init.sh
@@ -1,27 +1,22 @@
-if [ $# -ne 4 ]
-then
-  echo "Usage: `basename $0` SERVER_NAME SERVER_ADDRESS SITE_DOMAIN SITE_GATEWAY"
-  exit -1
-fi
+if [ -z "$SERVER_NAME" ]; then echo "export SERVER_NAME"; exit 11; fi
+if [ -z "$SERVER_ADDRESS" ]; then echo "export SERVER_ADDRESS"; exit 11; fi
+if [ -z "$SITE_DOMAIN" ]; then echo "export SITE_DOMAIN"; exit 11; fi
+if [ -z "$SITE_GATEWAY" ]; then echo "export SITE_GATEWAY"; exit 11; fi
 
-SERVER_NAME=$1
-SERVER_ADDRESS=$2
-SITE_DOMAIN=$3
-SITE_GATEWAY=$4
-
-echo $SERVER_NAME > /etc/hostname
+echo $SERVER_NAME | sudo tee /etc/hostname
 
 sudo aptitude install -y bridge-utils
 
 INFCSFNAME=/etc/network/interfaces
 sudo cp interfaces.pref $INFCSFNAME
-sed "s/{{ GATEWAY }}/$SITE_GATEWAY" -i $INFCSFNAME
-sed "s/{{ SERVERIP }}/$SERVER_ADDRESS" -i $INFCSFNAME
-sed "s/{{ SITE_DOMAIN }}/$SITE_DOMAIN" -i $INFCSFNAME
+sudo sed "s/{{ GATEWAY }}/$SITE_GATEWAY/g" -i $INFCSFNAME
+sudo sed "s/{{ SERVERIP }}/$SERVER_ADDRESS/g" -i $INFCSFNAME
+sudo sed "s/{{ SITE_DOMAIN }}/$SITE_DOMAIN/g" -i $INFCSFNAME
 
 # remove waiting for network
-sed 's/sleep 40/#sleep 40' -i /etc/init/failsafe.conf
-sed 's/sleep 59/#sleep 59' -i /etc/init/failsafe.conf
+sudo sed 's/sleep 40/#sleep 40/g' -i /etc/init/failsafe.conf
+sudo sed 's/sleep 59/#sleep 59/g' -i /etc/init/failsafe.conf
 
-echo "nameserver $SERVER_ADDRESS" >> /etc/resolvconf/resolv.conf.d/base
-echo "domain $SITE_DOMAIN" >> /etc/resolvconf/resolv.conf.d/base
+# setup DNS resolver
+echo "nameserver $SERVER_ADDRESS" | sudo tee /etc/resolvconf/resolv.conf.d/base
+echo "domain $SITE_DOMAIN" | sudo tee /etc/resolvconf/resolv.conf.d/base
