17211754f2fefc64bace73e36a36705bf2cc0b90
Vaclav Klecanda <vencax77@gmail.com>
2014-01-15 11:30:59 +0100

    sql

---
+++ sql/auth/client-stuff.sh
@@ -0,0 +1,21 @@
+if [ -z "$SERVER_NAME" ]; then echo "export SERVER_NAME"; exit 11; fi
+if [ -z "$USERDB_USERNAME" ]; then echo "export USERDB_USERNAME"; exit 11; fi
+if [ -z "$USERDB_PWD" ]; then echo "export USERDB_PWD"; exit 11; fi
+if [ -z "$USERDB_NAME" ]; then echo "export USERDB_NAME"; exit 11; fi
+
+aptitude install -y libnss-mysql-bg mysql-client
+
+NSSWITCH_CONF_FILE=/etc/nsswitch.conf
+sudo sed 's/compat/compat mysql/g' -i $NSSWITCH_CONF_FILE
+
+CFGFILE=/etc/libnss-mysql.cfg
+sudo cp libnss-mysql.cfg.pref $confFile
+sudo sed "s/{{ SERVER_NAME }}/$SERVER_NAME/g" -i $CFGFILE
+sudo sed "s/{{ DB_NAME }}/$USERDB_NAME/g" -i $CFGFILE
+sudo sed "s/{{ DB_USERNAME }}/$USERDB_USERNAME/g" -i $CFGFILE
+sudo sed "s/{{ DB_PWD }}/$USERDB_PWD/g" -i $CFGFILE
+
+CFGFILE=/etc/libnss-mysql-root.cfg
+echo "username  $USERDB_USERNAME" | sudo tee $CFGFILE
+echo "password  $USERDB_PWD" | sudo tee -a $CFGFILE
+chmod 600 $CFGFILE
---
+++ sql/server.sh
@@ -0,0 +1,21 @@
+if [ -z "$USERDB_USERNAME" ]; then echo "export USERDB_USERNAME"; exit 11; fi
+if [ -z "$USERDB_PWD" ]; then echo "export USERDB_PWD"; exit 11; fi
+if [ -z "$USERDB_NAME" ]; then echo "export USERDB_NAME"; exit 11; fi
+
+sudo aptitude install -y mysql-server
+# libmysqlclient-dev
+
+CFGFILE=/etc/mysql/my.cnf
+sudo sed "s/127.0.0.1/0.0.0.0/g" -i $CFGFILE
+sudo service mysql restart
+
+sudo mysqladmin -p create $USERDB_NAME
+T=/tmp/uuuuu
+cat > $T <<EOF
+CREATE USER $USERDB_USERNAME IDENTIFIED BY '$USERDB_PWD';
+GRANT ALL ON $USERDB_NAME.* TO '$USERDB_USERNAME'@'%' IDENTIFIED BY '$USERDB_PWD';
+GRANT ALL ON $USERDB_NAME.* TO '$USERDB_USERNAME'@'localhost' IDENTIFIED BY '$USERDB_PWD';
+FLUSH PRIVILEGES;
+EOF
+sudo mysql -u root -p < $T
+rm $T
