891b0d4596c1f7c88d3775dd7351b2530ff75899
Vaclav Klecanda <vencax77@gmail.com>
2014-06-15 14:36:19 +0200

    env var renamed

--- networking/dhcp/install_server.sh
+++ networking/dhcp/install_server.sh
@@ -1,7 +1,7 @@
 if [ -z "$SITE_DOMAIN" ]; then echo "export SITE_DOMAIN"; exit 11; fi
 if [ -z "$SERVER_ADDRESS" ]; then echo "export SERVER_ADDRESS"; exit 11; fi
 if [ -z "$SITE_GATEWAY" ]; then echo "export SITE_GATEWAY"; exit 11; fi
-if [ -z "$NETWORKPART" ]; then echo "export NETWORKPART"; exit 11; fi
+if [ -z "$NETPART" ]; then echo "export NETPART"; exit 11; fi
 if [ -z "$REVNETWORK" ]; then echo "export REVNETWORK"; exit 11; fi
 
 ROOT=/etc/dhcp
@@ -13,7 +13,7 @@ sudo cp dhcpd.conf.pref $ROOT/dhcpd.conf
 sudo sed "s/{{ DOMAIN }}/$SITE_DOMAIN/g" -i $CNF
 sudo sed "s/{{ SERVERIP }}/$SERVER_ADDRESS/g" -i $CNF
 sudo sed "s/{{ ROUTER }}/$SITE_GATEWAY/g" -i $CNF
-sudo sed "s/{{ NETWORKPART }}/$NETWORKPART/g" -i $CNF
+sudo sed "s/{{ NETWORKPART }}/$NETPART/g" -i $CNF
 sudo sed "s/{{ REVNETWORK }}/$REVNETWORK/g" -i $CNF
 
 SECRET=`sudo cat /etc/bind/rndc.key | grep secret | cut -d ' ' -f 2`
