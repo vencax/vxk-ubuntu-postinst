afb60bf1e61f97eaf7ce94e5daa820d706e76c25
Vaclav Klecanda <vencax77@gmail.com>
2014-06-23 21:15:42 +0200

    fixes

--- admin_app/init.sh
+++ admin_app/init.sh
@@ -3,40 +3,44 @@ if [ -z "$USERDB_PWD" ]; then echo "export USERDB_PWD"; exit 11; fi
 if [ -z "$COMMANDER" ]; then echo "export COMMANDER"; exit 11; fi
 if [ -z "$SITE_DOMAIN" ]; then echo "export SITE_DOMAIN"; exit 11; fi
 
+PWDBACK=`pwd`
 ROOT=/var/wsgi
 PROJROOT=$ROOT/s21
 PROJURL=https://github.com/vencax/py-s21-admin
 UWSGIROOT=/etc/uwsgi
 U=www-data
 
-sudo aptitude install -y uwsgi uwsgi-plugin-python \ 
-	python-setuptools \
+sudo aptitude install -y uwsgi uwsgi-plugin-python \
+	python-setuptools python-virtualenv \
 	build-essential python-dev libmysqlclient-dev
 sudo easy_install pip
 
-mkdir $ROOT
-chown $U:$U $ROOT
+sudo mkdir $ROOT
+sudo chown $USER:$U $ROOT
 
-sudo -u $U "git clone $PROJURL $PROJROOT"
+cd $ROOT
+git clone $PROJURL $PROJROOT
 cd $PROJROOT
+virtualenv venv
 
-pip install -r requirements.txt
+venv/bin/pip install -r requirements.txt
 
 # commander user creation
 sudo useradd $COMMANDER
-echo "$COMMANDER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/commander
-chmod 0440 /etc/sudoers.d/commander
-COMMANDERSECRET=`openssl rand -base64 32`
+echo "$COMMANDER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/commander
+sudo chmod 0440 /etc/sudoers.d/commander
+COMMANDERSECRET=`openssl rand -base64 32 | sed 's/\//r/'`
 sudo passwd $COMMANDER <<EOF
 $COMMANDERSECRET
 $COMMANDERSECRET
 EOF
 
+cd $PWDBACK
 CFGFILE=$UWSGIROOT/apps-available/s21.ini
 sudo cp conf.ini.pref $CFGFILE
 sudo ln -s $UWSGIROOT/apps-available/s21.ini $UWSGIROOT/apps-enabled/s21.ini
 
-DJSECRET=`openssl rand -base64 32`
+DJSECRET=`openssl rand -base64 32 | sed 's/\//r/'`
 sudo sed "s/{{ DJANGO_SECRET }}/$DJSECRET/g" -i $CFGFILE
 sudo sed "s/{{ DBUSER }}/$USERDB_USERNAME/g" -i $CFGFILE
 sudo sed "s/{{ DBPASS }}/$USERDB_PWD/g" -i $CFGFILE
@@ -44,19 +48,26 @@ sudo sed "s/{{ COMMAND_TARGET_USER }}/$COMMANDER/g" -i $CFGFILE
 sudo sed "s/{{ COMMAND_TARGET_PASSWD }}/$COMMANDERSECRET/g" -i $CFGFILE
 
 cd $PROJROOT
-sudo -u $U cat <<EOL
+ENVFILE=.env
+cat <<EOF > $ENVFILE
 export DJANGO_SECRET=$DJSECRET
 export DBUSER=$USERDB_USERNAME
 export DBPASS=$USERDB_PWD
-EOL > .env
+export COMMAND_TARGET_USER=$COMMANDER
+export COMMAND_TARGET_PASSWD=$COMMANDERSECRET
+EOF
+#sudo chown root $ENVFILE
+#sudo chmod 600 $ENVFILE
 
-sudo -u $U source .env && python manage.py collectstatics -l -y
-sudo -u $U source .env && python manage.py syncdb
+sudo service uwsgi restart
+
+source .env && venv/bin/python manage.py collectstatic -l --noinput
+source .env && venv/bin/python manage.py syncdb
 
 # install nginx
 sudo aptitude install -y nginx
 
-CFGFILE=/etc/nginx/sites-available/s21.conf
+export CFGFILE=/etc/nginx/sites-available/s21.conf
 sudo cp nginx.conf.pref $CFGFILE
 sudo sed "s/{{ DOMAIN }}/$SITE_DOMAIN/g" -i $CFGFILE
 cd /etc/nginx/sites-enabled
