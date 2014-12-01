fcc598acb0c532f26ea3f07c31d0cde6e3a46ae1
Vaclav Klecanda <vencax77@gmail.com>
2014-12-01 08:38:37 +0100

    node stuff

---
+++ admin_app/node.sh
@@ -0,0 +1,38 @@
+
+if [ -z "$SERVERSECRET" ]; then echo "export SERVERSECRET"; exit 11; fi
+
+NODEAPPDIR="/var/node"
+
+function nvm() {
+  NVM_DIR="/usr/local/nvm"
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.20.0/install.sh | bash
+}
+
+function addNodeInstance(instanceName) {
+  $CFGFILE=/etc/init.d/$instanceName
+  cp node_template.conf.pref $CFGFILE
+  sudo sed "s/{{ NAME }}/$instanceName/g" -i $CFGFILE
+  update-rc.d $instanceName defaults
+}
+
+function installDhcpdRest() {
+  APPNAME="dhcpd-rest"
+
+  cd $NODEAPPDIR
+  git clone https://github.com/vencax/node-dhcp-rest-conf $APPNAME
+  cd $APPNAME
+  npm install
+
+  echo "export DHCPD_CONF_FILE=/etc/dhcp/dhcpd.conf" > .env
+  echo "export DHCPD_LEASES_FILE=/var/lib/dhcp/dhcpd.leases" >> .env
+  echo "export PORT=7801" >> .env
+  echo "export RELOAD_DHCPD='service isc-dhcp-server reload'" >> .env
+  echo "export SERVER_SECRET=$SERVERSECRET" >> .env
+  #echo "export CORSORIGIN="192.168.1.1" >> .env
+
+  addNodeInstance($APPNAME)
+}
+
+nvm
+
+installDhcpdRest
