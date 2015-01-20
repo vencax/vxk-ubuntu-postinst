005e768df24a419672ab3979325fa444a90f2023
Vaclav Klecanda <vencax77@gmail.com>
2015-01-20 20:46:04 +0100

    auditlogs

--- samba/init.sh
+++ samba/init.sh
@@ -16,3 +16,35 @@ sudo sed "s/{{ SERVER_NAME }}/$SERVER_NAME/g" -i $CFG
 sudo sed "s/{{ SITE_DOMAIN }}/$SITE_DOMAIN/g" -i $CFG
 
 sudo cp -r netlogon /samba/shares
+
+function makereadonlyfolder() {
+  # set setGUID bit to make all files inherit group
+  chmod +s $1
+  # set sticky bit to prevent deletion by other users
+  chmod +t $1
+}
+
+function auditlog {
+
+  # setup local5 syslog facility for samba.audit.log
+  # nano /etc/rsyslog.d/50-default.conf
+  # add:
+  # local5.*                 /var/log/samba_audit.log
+  # append on line where is "-/var/log/syslog"
+  # local5.none
+
+  # yet setup logrotator
+  echo '/var/log/samba_audit.log {
+     weekly
+     missingok
+     rotate 7
+     postrotate
+          service rsyslog restart > /dev/null 2>&1 || true
+     endscript
+     compress
+     notifempty
+  }' | sudo tee /etc/logrotate.d/samba.audit
+
+}
+
+auditlog
