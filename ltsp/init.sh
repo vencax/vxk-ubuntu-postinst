a9f0e0205e1227f4c17b1afb5f62f9a06a84d46f
vencax <vencax77@gmail.com>
2015-05-25 08:56:31 +0200

    fix lubuntu desktop

--- ltsp/init.sh
+++ ltsp/init.sh
@@ -5,18 +5,20 @@ if [ -z "$SITE_GATEWAY" ]; then echo "export SITE_GATEWAY"; exit 11; fi
 
 sudo aptitude install -y ltsp-server syslinux
 
-# add neccesaary stuff into dhcp conf
-DHCPSTUFF='if substring( option vendor-class-identifier, 0, 9 ) = "PXEClient" {
-        filename "/ltsp/i386/pxelinux.0";
-} else {
-        filename "/ltsp/i386/nbi.img";
-}'
-CNF=/etc/dhcp/dhcpd.conf
-sed -e '/# {{ PXEBOOT_OPTS_HERE }}/,$d' $CNF > dhcp.conf.tmp
-echo "$DHCPSTUFF" >> dhcp.conf.tmp
-sed -e '1,/# {{ PXEBOOT_OPTS_HERE }}/d' $CNF >> dhcp.conf.tmp
-sudo mv dhcp.conf.tmp $CNF
-sudo service isc-dhcp-server restart
+function injectDhcpdConf() {
+  # add neccessary stuff into dhcp conf
+  DHCPSTUFF='if substring( option vendor-class-identifier, 0, 9 ) = "PXEClient" {
+          filename "/ltsp/i386/pxelinux.0";
+  } else {
+          filename "/ltsp/i386/nbi.img";
+  }'
+  CNF=/etc/dhcp/dhcpd.conf
+  sed -e '/# {{ PXEBOOT_OPTS_HERE }}/,$d' $CNF > dhcp.conf.tmp
+  echo "$DHCPSTUFF" >> dhcp.conf.tmp
+  sed -e '1,/# {{ PXEBOOT_OPTS_HERE }}/d' $CNF >> dhcp.conf.tmp
+  sudo mv dhcp.conf.tmp $CNF
+  sudo service isc-dhcp-server restart
+}
 
 CFGROOT=/etc/ltsp/
 sudo mkdir $CFGROOT
@@ -24,6 +26,14 @@ sudo cp ltsp-build-client.conf.pref $CFGROOT/ltsp-build-client.conf
 sudo cp ltsp-chroot.conf.pref $CFGROOT/ltsp-chroot.conf
 
 sudo ltsp-build-client --skipimage
+
+IMAGEROOT=/opt/ltsp
+# fix Lubuntu desktop
+sudo rm $IMAGEROOT/etc/X11/Xsession.d/40-ltsp-client
+#sudo rm $IMAGEROOT/etc/xdg/autostart/indicator-bluetooth.desktop
+#unnecessary nm-applet remove
+#sudo rm $IMAGEROOT/etc/xdg/autostart/nm-applet.desktop
+
 
 TFTPROOT=/var/lib/tftpboot/ltsp/i386
 sudo cp /usr/lib/syslinux/menu.c32 $TFTPROOT
